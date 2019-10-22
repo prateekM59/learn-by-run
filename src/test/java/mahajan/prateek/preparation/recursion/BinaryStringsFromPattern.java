f23c81e51394f3447a3cdc7d6e30e15cd69e19bf
Prateek Mahajan <pramahajan@egencia.com>
2019-10-23 03:17:22 +0530

    Adding recursion problems - 1

---
+++ src/test/java/mahajan/prateek/preparation/recursion/BinaryStringsFromPattern.java
@@ -0,0 +1,42 @@
+package mahajan.prateek.preparation.recursion;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by: pramahajan on 10/23/19 3:13 AM GMT+05:30
+ */
+public class BinaryStringsFromPattern {
+
+
+    @Test
+    // https://www.techiedelight.com/find-binary-strings-can-formed-given-wildcard-pattern/
+    public void print_all_binary_strings_generated_from_wildcard_pattern() {
+        String src = "1??0";
+        printAllCombinations(src.toCharArray(), src.length()-1);
+    }
+
+
+    private void printAllCombinations(char pattern[], int i)
+    {
+        if (i > 0) {
+            //System.out.println("i: " + i + Arrays.toString(pattern));
+        } else if (i==0) {
+            System.out.println("i: " + i + Arrays.toString(pattern));
+            return;
+        }
+
+        if (pattern[i] == '?') {
+            pattern[i] = '0';
+            printAllCombinations(pattern, i - 1);
+
+            pattern[i] = '1';
+            printAllCombinations(pattern, i - 1);
+
+            pattern[i] = '?'; // revert for further recursion
+        } else {
+            printAllCombinations(pattern, i - 1);
+        }
+    }
+}
---
+++ src/test/java/mahajan/prateek/preparation/recursion/NonOverlappingSubstrings.java
@@ -0,0 +1,40 @@
+package mahajan.prateek.preparation.recursion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Stack;
+
+/**
+ * Created by: pramahajan on 10/23/19 3:09 AM GMT+05:30
+ */
+
+@RunWith(JUnit4.class)
+
+public class NonOverlappingSubstrings {
+
+    @Test
+    // https://www.techiedelight.com/find-combinations-non-overlapping-substrings-string/
+    public void find_all_non_overlapping_substrings() {
+        String src = "ABC";
+        overlapSubstring(new Stack<>(), src);
+    }
+
+
+    private void overlapSubstring(Stack<String> res, String str) {
+        if (str.length() == 0) {
+            System.out.println(res);
+            return;
+        }
+        int n = str.length();
+        for (int i=0; i<n;i++) {
+            String sub = str.substring(0,i+1);
+            //System.out.println("Sub: " + sub);
+            res.push(sub);
+            overlapSubstring(res, str.substring(i+1, n));
+            res.pop();
+        }
+    }
+
+}
